1216265d018324b379d8ea531dae8123eec41ca6
mateuswillian <145728223+mateuswillian@users.noreply.github.com>
2024-01-10 18:51:49 -0300

    aula do dia 01.09

--- aula01.08/menuDoJogo.js
+++ aula01.08/menuDoJogo.js
@@ -3,8 +3,19 @@
 
 const prompt = require ('prompt-sync')()
 
-const options = prompt ("Escolha: 1 ( New Game) , 2 (Load Game) , 3(Settings) , 4(Quit)") 
+const options = Number (prompt ("Escolha: 1 ( New Game) , 2 (Load Game) , 3(Settings) , 4(Quit) "))
 
 switch (options) {
-    case
+    case 1:
+        console.log('Bem vindo ao novo jogo')
+        break
+    case 2:
+        console.log('Aguarde um pouco, jogo em andamento...')
+        break
+    case 3:
+        console.log('Controle / som / dicas / voltar ao jogo')
+        break
+    case 4:
+        console.log('Bem vindo ao jogo. Você deseja: Continuar / novo jogo')
+        break
 }
