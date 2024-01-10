497a5308af49c20b1d9a6010ca252af067100983
mateuswillian <145728223+mateuswillian@users.noreply.github.com>
2024-01-10 18:56:33 -0300

    aula do dia 10.01

---
+++ aula01.10/acerteOnumero.js
@@ -0,0 +1,4 @@
+// crie um game para que o usuario ficar tentanto acertar o numero oculto. quando ele acertar o game deve finalizar e informar a quantidade de tentativas.
+
+const prompt = require ('prompt-sync')()
+
