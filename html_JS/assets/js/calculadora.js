75b28f4cfa24aa90c5f6c32f6e2e44e1e58cbdec
mateuswillian <145728223+mateuswillian@users.noreply.github.com>
2024-01-23 19:28:40 -0300

    calculadora finalizada

---
+++ html_JS/assets/js/calculadora.js
@@ -0,0 +1,20 @@
+function calcular(tipo, valor){
+    console.log(`O tipo é ${tipo} e o valor é: ${valor}`)
+
+    if (tipo === 'num') {
+        document.getElementById('resultado').value += valor
+    } else if(tipo === 'acao') {
+        if( valor === 'c') {
+            document.getElementById('resultado').value = ''
+        }
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor
+        }
+
+        if (valor ===  '=') {
+            let valorCampo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valorCampo
+        }
+    }
+    
+}
